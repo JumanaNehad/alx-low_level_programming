be8397e9c4e45eea3e7c0253cd10b3de44d70b7b
jumana <jumana1903964@miuegypt.edu.eg>
2023-08-25 03:51:53 +0300

    m

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
@@ -14,3 +23,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
